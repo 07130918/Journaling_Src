5a707a67725ba32cc5392febb1776b41818e1b32
こた <tokyosato1@gmail.com>
2023-02-11 19:19:49 +0900

    reat: add os.chdir(DIR_LOCATION)

--- done.py
+++ done.py
@@ -7,6 +7,7 @@ from consts import DIR_LOCATION
 def main():
     """Journalingを書き終わった後にpushまで行う関数
     """
+    os.chdir(DIR_LOCATION)
     os.system(f'git add {DIR_LOCATION}/.')
     os.system(f'git commit -m {datetime.now().strftime("%Y/%m/%d")}')
     os.system('git push')
