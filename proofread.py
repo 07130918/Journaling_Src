24b80a65bcad3a26c86c05a9b560c2f630e3316b
こた <tokyosato1@gmail.com>
2023-05-28 00:11:32 +0900

    校正を行うプログラム作成

---
+++ proofread.py
@@ -0,0 +1,81 @@
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+
+JOURNALING_DIR = "/Users/satokota/Desktop/English/Journaling"
+NOW = datetime.now()
+CURRENT_YEAR = NOW.strftime("%Y")
+CURRENT_MONTH_NAME = NOW.strftime("%B")
+TODAY = (f"{CURRENT_YEAR}/{CURRENT_MONTH_NAME}/{str(NOW.strftime('%d')).zfill(2)}")
+# 学習したい言語
+LANGUAGE = "English"
+
+def main():
+    """proofread journaling
+
+    Raises:
+        FileNotFoundError: if journaling file is not found
+
+    Returns:
+        str: response from GPT-3
+        None: if journaling file is not found
+    """
+    load_dotenv()
+    print("Loading...")
+    os.environ["OPENAI_API_KEY"] = os.environ['OPENAI_API_KEY']
+    try:
+        with open(f"{JOURNALING_DIR}/{TODAY}.txt", "r") as f:
+            journal = f.read()
+    except FileNotFoundError as e:
+        print(e)
+        print("今日の日記はまだ書いていません。")
+        return None
+
+    assistant_template = "You are a helpful and capable assistant who can behave as a native {language} speaker."
+    user_template = """
+    I'm looking for someone who can play the role of a native {language} speaker and teacher.
+    I'm Japanese and currently learning {language} as my second language.
+    I've written a journal and I'd appreciate it if you could review it and correct it to sound more natural and accurate.
+
+    Also, You must write each sentence on a new line.
+
+    For example, if the sentences are like this:
+    ###
+    I want to improve my English skills. Besides, I would love to have friends who speak fluent English.
+    ###
+
+    I'd like your response to be something like this:
+    ###
+    I'm eager to enhance my English proficiency.
+
+    Additionally, I'd also really enjoy making friends with those who are fluent in English.
+    ###
+
+    Today's journal: {journal}
+    Your answer:
+    """
+
+    # クリエイティブな文章を生成するため、temperatureを1に設定
+    # modelはgpt-4に変更予定
+    gpt = ChatOpenAI(model="gpt-3.5-turbo", temperature=1, client=None)
+
+    assistant_prompt = SystemMessagePromptTemplate.from_template(assistant_template)
+    user_prompt = HumanMessagePromptTemplate.from_template(user_template)
+    chat_prompt = ChatPromptTemplate.from_messages([assistant_prompt, user_prompt])
+
+    chain = LLMChain(llm=gpt, prompt=chat_prompt)
+    response = chain.run(language=LANGUAGE, journal=journal)
+    print("-" * 20)
+    print(response)
+    print("-" * 20)
+    return response
+
+
+if __name__ == '__main__':
+    main()
