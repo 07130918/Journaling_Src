686996010798832172ce4c6a7815e45a3fcb9039
こた <tokyosato1@gmail.com>
2023-08-06 12:00:27 +0900

    fix change model

--- proofread.py
+++ proofread.py
@@ -62,7 +62,7 @@ def main():
 
     # temperatureは値を変更していろいろ試したけど、0.8が良さそう。
     # modelはgpt-4に変更予定
-    gpt = ChatOpenAI(model="gpt-4-0613", temperature=.8, client=None)
+    gpt = ChatOpenAI(model="gpt-3.5-turbo", temperature=.8, client=None)
 
     assistant_prompt = SystemMessagePromptTemplate.from_template(assistant_template)
     user_prompt = HumanMessagePromptTemplate.from_template(user_template)
